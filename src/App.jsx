0bd4f0a0889be210a93675486ddba2dcf31981b8
Samuel Pereira <gmr.quest.10@gmail.com>
2021-12-08 15:53:06 -0300

    Faz configuraçao das rotas e context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <Switch>
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
 
