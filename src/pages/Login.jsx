18f877eee8c7bf4853e5e4683fdf88d9e6960b6f
larymanzo <lamosinha@hotmail.com>
2021-12-08 18:56:58 -0300

    Requisito 6 ao 8

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import LoginForm from '../components/LoginForm';
+import { saveLocalStorage } from '../helpers/manageLocalStorage';
 
 const SIX = 6;
 
-export default function Login() {
+export default function Login({ history }) {
   const [loginInfo, setLoginInfo] = useState({ email: '', password: '' });
   const [isSubmitBtnDisabled, setIsSubmitBtnDisabled] = useState(true);
 
@@ -22,13 +24,30 @@ export default function Login() {
     setLoginInfo((prevState) => ({ ...prevState, [id]: value }));
   };
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    saveLocalStorage('mealsToken', 1);
+    saveLocalStorage('cocktailsToken', 1);
+    saveLocalStorage('user', { email: loginInfo.email });
+
+    history.push('/comidas');
+  };
+
   return (
     <div className="login-form">
       <LoginForm
         loginInfo={ loginInfo }
         handleChange={ handleLoginChange }
         isSubmitBtnDisabled={ isSubmitBtnDisabled }
+        handleFormSubmit={ handleFormSubmit }
       />
     </div>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+
+};
