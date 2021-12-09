964fc3051bc1db82b5430fd1c4834a0525d54368
larymanzo <lamosinha@hotmail.com>
2021-12-09 16:44:12 -0300

    Rotas prontas

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import DetailRecipe from './pages/DetailRecipe';
 import Drinks from './pages/Drinks';
 import Explore from './pages/Explore';
-import ExploreIngredients from './pages/ExploreIngredients';
+import ExploreMealsIngredients from './pages/ExploreMealsIngredients';
 import ExploreLocal from './pages/ExploreLocal';
-import ExploreMealsDrinks from './pages/ExploreMealsDrinks';
+import ExploreDrinksIngredients from './pages/ExploreDrinksIngredients';
 import FavoritesRecipes from './pages/FavoritesRecipes';
 import Login from './pages/Login';
 import MainRecipe from './pages/MainRecipe';
 import Meals from './pages/Meals';
 import Profile from './pages/Profile';
-import RecipeInProgress from './pages/RecipeInProgress';
 import RecipeMade from './pages/RecipeMade';
 
 function App() {
   return (
     <Switch>
+      <Route path="/receitas-principais" component={ MainRecipe } />
+      <Route path="/receitas-feitas" component={ RecipeMade } />
+      <Route path="/receitas-favoritas" component={ FavoritesRecipes } />
+      <Route path="/explorar/comidas/area" component={ ExploreLocal } />
+      <Route
+        path="/explorar/comidas/ingredientes"
+        component={ ExploreMealsIngredients }
+      />
+      <Route
+        path="/explorar/bebidas/ingredientes"
+        component={ ExploreDrinksIngredients }
+      />
       <Route path="/profile" component={ Profile } />
       <Route path="/comidas" component={ Meals } />
-      <Route path="/comidas" component={ Drinks } />
-      <Route path="/comidas" component={ Explore } />
-      <Route path="/comidas" component={ MainRecipe } />
-      <Route path="/comidas" component={ DetailRecipe } />
-      <Route path="/comidas" component={ ExploreIngredients } />
-      <Route path="/comidas" component={ ExploreLocal } />
-      <Route path="/comidas" component={ ExploreMealsDrinks } />
-      <Route path="/comidas" component={ FavoritesRecipes } />
-      <Route path="/comidas" component={ RecipeInProgress } />
-      <Route path="/comidas" component={ RecipeMade } />
+      <Route path="/bebidas" component={ Drinks } />
+      <Route path="/explorar" component={ Explore } />
       <Route path="/" component={ Login } />
     </Switch>
   );
