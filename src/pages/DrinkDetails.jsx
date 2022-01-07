0f21166a451ffab415f08c1add573fec89a64572
Erickson <ericksonsiqueirasilva@gmail.com>
2022-01-07 18:32:41 -0300

    Tirando console.log

--- src/pages/DrinkDetails.jsx
+++ src/pages/DrinkDetails.jsx
@@ -19,8 +19,6 @@ function DrinkDetails() {
 
   const history = useHistory();
 
-  console.log(drink);
-
   const MAX_RECOMMENDATION = 6;
   const recommendations = meals.slice(0, MAX_RECOMMENDATION);
 
