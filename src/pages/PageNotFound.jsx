6de62fcc940a39eab263fd0d42efd5610c2d1c22
Joao Marco <joaomarco.matta@gmail.com>
2022-01-15 17:46:54 -0300

    Estiliza tela not found.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import octopusImg from '../images/notFound.svg';
 
 function PageNotFound() {
+  const history = useHistory();
+
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="custom-h-screen flex flex-col justify-evenly items-center">
+      <h1 className="text-center font-palanquin font-bold text-2xl text-titles-text">Erro 404</h1>
+      <div>
+        <img src={ octopusImg } alt="" />
+      </div>
+      <h1 className="text-center font-palanquin font-bold text-2xl text-not-found-text">Oops! Algo deu errado...</h1>
+      <p className="text-justify px-8 -mt-5">
+        Parece que tivemos um problema durante o carregamento. Volte para a página inicial e tente novamente.
+      </p>
+      <button
+        type="submit"
+        data-testid="back-btn"
+        className="salmon-btn shadow-lg shadow-btn-shadow hover:shadow-menu-itens "
+        onClick={ () => history.goBack() }
+      >
+        Voltar
+      </button>
     </div>
   );
 }
