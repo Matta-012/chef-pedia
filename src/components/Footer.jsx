825a7af05b6c8202241a5afa0e95da3508dd98a5
Samuel Pereira <gmr.quest.10@gmail.com>
2022-01-15 18:52:54 -0300

    Estilizaçao

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,17 @@ import mealIconSelected from '../images/mealIconSelected.svg';
 import drinkIcon from '../images/drinkIcon.svg';
 import drinkIconSelected from '../images/drinkIconSelected.svg';
 import exploreIcon from '../images/exploreIcon.svg';
-import exploreIconSelected from '../images/exploreIconSelected.svg';
-import '../styles/footer.css';
+import exploreIconSelected from '../images/exploreIconSelected.svg';  
 
 export default function Footer() {
   const {pathname} = useLocation();
 
   return (
-    <footer data-testid="footer">
-      <nav className="footer-links bg-white py-2 border border-t-gray-300">
+    <footer
+      data-testid="footer"
+      className="fixed bottom-0 w-full"
+    >
+      <nav className="flex justify-around bg-white py-2 border border-t-gray-300 rounded-t-lg">
         <Link to="/comidas">
           <div>
             {pathname === '/comidas'
