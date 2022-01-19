6b616497ca7d890460edcee9fc7ac7a00e5ca395
Erickson <ericksonsiqueirasilva@gmail.com>
2022-01-18 23:00:58 -0300

    Corrigindo crash ao voltar na tela de progresso de uma receita feita

--- src/pages/MealsInProgress.jsx
+++ src/pages/MealsInProgress.jsx
@@ -8,6 +8,7 @@ import toggleFinishButton from '../helpers/foodInProgressHelpers';
 import GoBackTop from '../components/GoBackTop';
 import '../styles/default-font.css';
 import shareIcon from '../images/shareIcon.svg';
+import Footer from '../components/Footer';
 
 function MealInProgress() {
   const { pathname } = useLocation();
@@ -101,6 +102,23 @@ function MealInProgress() {
 
     history.push('/receitas-feitas');
   };
+
+  const doneRecipes = getLocalStorage('doneRecipes');
+
+  if (doneRecipes) {
+    const doneRecipe = doneRecipes.some(recipe => recipe.id === id);
+    if (doneRecipe) {
+      return (
+        <div className="container">
+          <GoBackTop pageName="Receita feita"/>
+          <h1>
+            Essa receita já foi feita.
+          </h1>
+          <Footer />
+        </div>
+      )
+    }
+  }
 
   return (
     <main className="font-wrapper">
